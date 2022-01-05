f24ddae0a6ea55c36cc517eb940964ee40f4b3e3
Kodemon <dev@kodemon.net>
2022-01-05 13:12:16 +0000

    Add docusaurus website

---
+++ website/docs/access/quick-start.md
@@ -0,0 +1,17 @@
+---
+sidebar_position: 2
+---
+
+# Quick Start
+
+Creating an access group.
+
+```ts
+import { Access, container } from "@valkyr/access";
+
+class AccountAccess extends Access<Permissions> {}
+
+export async function getAccountAccess(accountId: string, db = container.get("Database")) {
+  return new AccountAccess(await db.getPermissions(accountId, accountId));
+}
+```
---
+++ website/docusaurus.config.js
@@ -0,0 +1,153 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'Valkyr',
+  tagline: 'One way to build applications',
+  url: 'https://your-docusaurus-test-site.com',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'kodemon/valkyr', // Usually your GitHub org/user name.
+  projectName: 'valkyr', // Usually your repo name.
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: 'Valkyr',
+        logo: {
+          alt: 'My Site Logo',
+          src: 'img/logo.svg',
+        },
+        items: [
+          {
+            type: 'doc',
+            docId: 'access/overview',
+            position: 'left',
+            label: 'Access',
+          },
+          {
+            type: 'doc',
+            docId: 'auth/overview',
+            position: 'left',
+            label: 'Auth',
+          },
+          {
+            type: 'doc',
+            docId: 'db/overview',
+            position: 'left',
+            label: 'DB',
+          },
+          {
+            type: 'doc',
+            docId: 'event-store/overview',
+            position: 'left',
+            label: 'Event Store',
+          },
+          {
+            type: 'doc',
+            docId: 'router/overview',
+            position: 'left',
+            label: 'Router',
+          },
+          {
+            type: 'doc',
+            docId: 'server/overview',
+            position: 'left',
+            label: 'Server',
+          },
+          {
+            type: 'doc',
+            docId: 'socket/overview',
+            position: 'left',
+            label: 'Socket',
+          },
+          {
+            href: 'https://github.com/kodemon/valkyr',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Docs',
+            items: [
+              {
+                label: 'Tutorial',
+                to: '/docs/intro',
+              },
+            ],
+          },
+          {
+            title: 'Community',
+            items: [
+              {
+                label: 'Stack Overflow',
+                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+              },
+              {
+                label: 'Discord',
+                href: 'https://discordapp.com/invite/docusaurus',
+              },
+              {
+                label: 'Twitter',
+                href: 'https://twitter.com/docusaurus',
+              },
+            ],
+          },
+          {
+            title: 'More',
+            items: [
+              {
+                label: 'Blog',
+                to: '/blog',
+              },
+              {
+                label: 'GitHub',
+                href: 'https://github.com/facebook/docusaurus',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
