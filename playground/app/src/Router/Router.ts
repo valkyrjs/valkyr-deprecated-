39eab41e770fa55e03fd75726d91af7298cc6144
Kodemon <dev@kodemon.net>
2022-02-04 04:10:14 +0900

    Correct import locations

--- playground/app/src/Router/Router.ts
+++ playground/app/src/Router/Router.ts
@@ -1,10 +1,9 @@
 import { createBrowserHistory, Router } from "@valkyr/router";
-
-export const router = new Router(createBrowserHistory());
-
 import { Route } from "@valkyr/router";
 
 import { Home } from "../Pages/Home";
 import { renderAuthorized } from "./Actions/RenderAuthorized";
+
+export const router = new Router(createBrowserHistory());
 
 router.register([new Route("/", [renderAuthorized([Home], "Home")])]);
