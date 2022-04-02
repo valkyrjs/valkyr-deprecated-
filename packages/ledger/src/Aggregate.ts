aa9070a7871e9ba79a0bee24feeb04f2f7b3dcce
Kodemon <dev@kodemon.net>
2022-04-03 02:48:23 +0900

    Add aggregate

--- packages/ledger/src/Aggregate.ts
+++ packages/ledger/src/Aggregate.ts
@@ -1,5 +1,9 @@
 import { Event } from "./Event";
 
+type Entity = {
+  id: string;
+};
+
 export type AggregateRootClass<T extends AggregateRoot = AggregateRoot> = {
   new (): T;
 };
@@ -7,4 +11,38 @@ export type AggregateRootClass<T extends AggregateRoot = AggregateRoot> = {
 export abstract class AggregateRoot {
   public abstract apply(event: Event): void;
   public abstract toJSON(): any;
+}
+
+export abstract class Aggregate<T extends Entity> {
+  public store = new Map<string, T>();
+
+  public get index() {
+    return Array.from(this.store.values());
+  }
+
+  public add(entity: T) {
+    this.store.set(entity.id, entity);
+  }
+
+  public get(id: string) {
+    return this.store.get(id);
+  }
+
+  public update(id: string, entity: Partial<T>) {
+    const stored = this.get(id);
+    if (stored) {
+      this.store.set(id, {
+        ...stored,
+        ...entity
+      });
+    }
+  }
+
+  public remove(id: string) {
+    this.store.delete(id);
+  }
+
+  public toJSON() {
+    return this.index;
+  }
 }
