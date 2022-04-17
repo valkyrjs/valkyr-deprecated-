5760dcfad03b718a6d75489db104dc42f541de99
Kodemon <dev@kodemon.net>
2022-04-17 14:16:05 +0900

    Validate auditor

--- packages/nestjs/src/Decorators/Auditor.ts
+++ packages/nestjs/src/Decorators/Auditor.ts
@@ -1,16 +1,21 @@
-import { createParamDecorator, ExecutionContext } from "@nestjs/common";
+import { BadRequestException, createParamDecorator, ExecutionContext, UnauthorizedException } from "@nestjs/common";
 import * as jwt from "jsonwebtoken";
 
 export const Auditor = createParamDecorator((data: unknown, ctx: ExecutionContext) => {
   const request = ctx.switchToHttp().getRequest();
   const [type, token] = request.headers?.authorization?.split(" ") ?? [];
 
-  if (type === "Bearer") {
-    const res = jwt.verify(token, "development");
-    if (typeof res !== "string") {
-      return res.auditor;
-    }
+  if (type === undefined) {
+    throw new UnauthorizedException();
   }
 
-  return "";
+  if (type !== "Bearer") {
+    throw new BadRequestException("Invalid authorization token provided");
+  }
+
+  const res = jwt.verify(token, "development");
+  if (typeof res === "string") {
+    throw new BadRequestException("Token verification failed");
+  }
+  return res.auditor;
 });
