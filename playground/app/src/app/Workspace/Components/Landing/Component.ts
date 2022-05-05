bdf6a5176a5e00cccbf03730e8fce9c31e501c53
mattgi <matt.ginty@gmail.com>
2022-05-05 16:08:53 +1000

    Add Masonry

--- playground/app/src/app/Workspace/Components/Landing/Component.ts
+++ playground/app/src/app/Workspace/Components/Landing/Component.ts
@@ -1,15 +1,43 @@
-import { Component } from "@angular/core";
+import { Component, Injector, OnInit } from "@angular/core";
+import { AuthService, SubscriptionDirective } from "@valkyr/angular";
 
+import { Workspace } from "../../Models/Workspace";
 import { WorkspaceService } from "../../Services/Workspace";
 
 @Component({
   selector: "workspace-landing",
   templateUrl: "./Template.html"
 })
-export class LandingComponent {
+export class LandingComponent extends SubscriptionDirective implements OnInit {
+  public workspaces: Workspace[] = [];
   public name = "";
 
-  constructor(private workspace: WorkspaceService) {}
+  constructor(private workspace: WorkspaceService, private auth: AuthService, injector: Injector) {
+    super(injector);
+  }
+
+  public ngOnInit(): void {
+    this.getWorkspaces();
+  }
+
+  public getWorkspaces() {
+    this.subscribe(
+      Workspace,
+      {
+        criteria: {
+          "members.accountId": this.auth.auditor
+        },
+        stream: {
+          aggregate: "workspace",
+          endpoint: "/workspaces"
+        }
+      },
+      (workspaces) => {
+        this.workspaces = workspaces;
+        console.log(this.workspaces);
+      }
+    );
+  }
 
   public create() {
     this.workspace.create(this.name).then(() => {
