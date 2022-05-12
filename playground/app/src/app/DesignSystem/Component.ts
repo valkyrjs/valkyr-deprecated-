be082313b737c254c0764731d89b6c45d46eca4b
mattgi <matt.ginty@gmail.com>
2022-05-12 11:38:38 +1000

    Add colors to style guide

--- playground/app/src/app/DesignSystem/Component.ts
+++ playground/app/src/app/DesignSystem/Component.ts
@@ -14,6 +14,26 @@ export class DesignSystemComponent {
     { variant: "negative", label: "Negative" }
   ];
 
+  public colors: { name: string; weights: number[] }[] = [
+    { name: "gray", weights: [50, 75, 100, 200, 300, 400, 500, 600, 700, 800, 900] },
+    { name: "celery", weights: [400, 500, 600, 700] },
+    { name: "chartreuse", weights: [400, 500, 600, 700] },
+    { name: "yellow", weights: [400, 500, 600, 700] },
+    { name: "magenta", weights: [400, 500, 600, 700] },
+    { name: "fuchsia", weights: [400, 500, 600, 700] },
+    { name: "purple", weights: [400, 500, 600, 700] },
+    { name: "indigo", weights: [400, 500, 600, 700] },
+    { name: "seafoam", weights: [400, 500, 600, 700] },
+    { name: "red", weights: [100, 400, 500, 600, 700] },
+    { name: "orange", weights: [400, 500, 600, 700] },
+    { name: "green", weights: [400, 500, 600, 700] },
+    { name: "blue", weights: [100, 400, 500, 600, 700] }
+  ];
+
+  public getClass(color: string, weight: number) {
+    return `bg-${color}-${weight} ${weight > 200 ? "text-white" : "text-black"}`;
+  }
+
   constructor(title: TitleService) {
     title.set("Design System", DOCUMENT_TITLE, "discovery");
   }
