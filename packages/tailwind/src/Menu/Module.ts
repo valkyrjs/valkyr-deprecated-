96bba42eb944ecd9a0aa34ba82119fce22875df2
mattgi <matt.ginty@gmail.com>
2022-05-16 20:12:09 +1000

    Lint

--- packages/tailwind/src/Menu/Module.ts
+++ packages/tailwind/src/Menu/Module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
-
 import { RouterModule } from "@angular/router";
 
 import { MenuCategoryComponent } from "./Category/Component";
