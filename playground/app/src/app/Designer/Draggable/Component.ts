b97b43d69b836c30bdd93f6ffca524722ac2869e
mattgi <matt.ginty@gmail.com>
2022-05-17 11:03:09 +1000

    Tidy up designer pov

--- playground/app/src/app/Designer/Draggable/Component.ts
+++ playground/app/src/app/Designer/Draggable/Component.ts
@@ -16,24 +16,10 @@ export class DraggableComponent {
   dragConstrainPoint = (point: any, dragRef: any) => {
     let zoomMoveXDifference = 0;
     let zoomMoveYDifference = 0;
-    console.log(
-      "freeDragPosition dragRef: " +
-        Math.round(dragRef.getFreeDragPosition().x) +
-        " / " +
-        Math.round(dragRef.getFreeDragPosition().y)
-    );
-
     if (this.zoomScale != 1) {
       zoomMoveXDifference = (1 - this.zoomScale) * dragRef.getFreeDragPosition().x;
       zoomMoveYDifference = (1 - this.zoomScale) * dragRef.getFreeDragPosition().y;
     }
-    console.log(
-      "zoomMoveXDifference x/y: " + Math.round(zoomMoveXDifference) + " / " + Math.round(zoomMoveYDifference)
-    );
-    console.log("Point x/y: " + Math.round(point.x) + " / " + Math.round(point.y));
-    console.log(
-      "Sum x/y: " + Math.round(point.x + zoomMoveXDifference) + " / " + Math.round(point.y + zoomMoveYDifference)
-    );
 
     return {
       x: point.x + zoomMoveXDifference,
@@ -42,33 +28,13 @@ export class DraggableComponent {
   };
 
   startDragging() {
-    console.log("START");
     this.dragStart.emit();
   }
 
   endDragging($event: any) {
-    console.log("END");
     const elementMoving = $event.source.getRootElement();
-    const elementMovingRect: ClientRect = elementMoving.getBoundingClientRect();
-    const elementMovingParentElementRect: ClientRect = elementMoving.parentElement.getBoundingClientRect();
-    /* The getBoundingClientRect() method returns the size of an element and its position relative to the viewport.
-    This method returns a DOMRect object with eight properties: left, top, right, bottom, x, y, width, height. https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_element_getboundingclientrect*/
-    console.log(
-      "elementMovingParentElementRect .left/.top: " +
-        Math.trunc(elementMovingParentElementRect.left) +
-        " / " +
-        Math.trunc(elementMovingParentElementRect.top)
-    );
-    console.log(
-      "elementMovingRect .left/.top: " + Math.trunc(elementMovingRect.left) + " / " + Math.trunc(elementMovingRect.top)
-    );
-    console.log(
-      "Difference scaled .left/.top: " +
-        Math.trunc((elementMovingRect.left - elementMovingParentElementRect.left) / this.zoomScale) +
-        " / " +
-        Math.trunc((elementMovingRect.top - elementMovingParentElementRect.top) / this.zoomScale)
-    );
-
+    const elementMovingRect = elementMoving.getBoundingClientRect();
+    const elementMovingParentElementRect = elementMoving.parentElement.getBoundingClientRect();
     this.pos.x = (elementMovingRect.left - elementMovingParentElementRect.left) / this.zoomScale;
     this.pos.y = (elementMovingRect.top - elementMovingParentElementRect.top) / this.zoomScale;
 
