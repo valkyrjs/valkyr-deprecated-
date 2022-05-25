b7209e21752b9f836d29eed19a967027b36b45d4
mattgi <matt.ginty@gmail.com>
2022-05-25 15:07:12 +1000

    Lint

--- playground/app/src/app/Library/Layout/Components/NavbarLinks/Component.ts
+++ playground/app/src/app/Library/Layout/Components/NavbarLinks/Component.ts
@@ -1,5 +1,5 @@
 import { Component, Input } from "@angular/core";
-import {  NavigationEnd, Router } from "@angular/router";
+import { NavigationEnd, Router } from "@angular/router";
 
 import { MenuItem } from "../../Models/MenuItem";
 
