f1c29afab8c083787a63bef05a48707322e18bd7
Kodemon <dev@kodemon.net>
2022-06-07 15:42:49 +0900

    Remove cucumber

--- cucumber.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  default: {
-    requireModule: ["ts-node/register"],
-    require: ["packages/**/tests/**/*steps.ts"],
-    paths: ["packages/**/features/**/*.feature"],
-    publishQuiet: true,
-    format:["progress-bar", "html:cucumber-report.html"]
-  }
-}
