567e805336127fd7a117489634ca9818f468c6a4
Kodemon <hi@kodemon.net>
2022-08-16 23:42:26 +0900

    chore(ledger-sync): remove unsupported package

--- packages/ledger-sync/src/EventCursor.ts
+++
@@ -1,3 +0,0 @@
-import { Storage } from "./Storage/Storage";
-
-export const cursors = new Storage<string>();
--- packages/ledger-sync/src/EventIndex.ts
+++
@@ -1,3 +0,0 @@
-import { Storage } from "./Storage/Storage";
-
-export const index = new Storage<true>();
