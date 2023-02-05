e75f0b3fed4e90ba11dd803fea18c9fdcac1b6af
kodemon <hi@kodemon.net>
2023-02-05 16:23:32 +0900

    chore(clean): remove genreated js files

--- templates/api/src/modules/ledger/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-/* eslint-disable simple-import-sort/imports */
-Object.defineProperty(exports, "__esModule", { value: true });
-/*
- |--------------------------------------------------------------------------------
- | Remote Procedure Calls
- |--------------------------------------------------------------------------------
- */
-require("./jsonrpc/event.methods");
--- templates/api/src/modules/ledger/ledger.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ledger = void 0;
-const event_store_1 = require("@valkyr/event-store");
-const database_1 = require("~services/database");
-exports.ledger = new event_store_1.EventStore(database_1.db.collection("events"));
